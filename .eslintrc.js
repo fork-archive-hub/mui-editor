6cef7e8b7f0353674f7ef1eae28ba3b1686f4e26
Yuanzong Qiu <congge@gmail.com>
2021-10-30 08:52:54 -0700

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
   ],
   plugins: ['react'],
   parserOptions: {
